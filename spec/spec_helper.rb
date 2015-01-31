e6baf6c09101f275287ce94745aa81367db6fb66
Keith Morrison <keithm@infused.org>
2015-01-31 12:14:36 -0800

    integrate with code climate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
 # encoding: ascii-8bit
 
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
+
 $:.unshift(File.dirname(__FILE__) + '/../lib/')
 require 'rubygems'
 require 'rspec'
