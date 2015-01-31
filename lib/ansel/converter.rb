1f953d34849d4161c122bf40174d4f28367163a0
Keith Morrison <keithm@infused.org>
2015-01-30 23:10:28 -0800

    reduce code complexity

--- lib/ansel/converter.rb
+++ lib/ansel/converter.rb
@@ -6,10 +6,12 @@ module ANSEL
 
     def initialize(to_charset = 'UTF-8')
       @to_charset = to_charset
+      @encoding_converter = Encoding::Converter.new("UTF-16BE", "UTF-8")
+      @ansi_to_utf16_map = @@non_combining.merge(@@combining)
     end
 
-    def ansi_to_utf16
-      @ansi_to_utf16 ||= @@non_combining.merge(@@combining)
+    def utf16_to_utf8(string)
+      @encoding_converter.convert(string)
     end
 
     def convert(string)
@@ -18,32 +20,32 @@ module ANSEL
       until scanner.eos? do
         byte = scanner.get_byte
         char = byte.unpack('C')[0]
+        char_hex = char.to_s(16).upcase
 
         case char
         when 0x00..0x7F
-          output << byte.force_encoding('UTF-8')
+          output << byte
         when 0x88..0xC8
-          hex_key = char.to_s(16).upcase
-          output << (ansi_to_utf16[hex_key] || ansi_to_utf16['ERR']).force_encoding('UTF-16BE').encode('UTF-8')
+          output << utf16_to_utf8(@ansi_to_utf16_map[char_hex] || @ansi_to_utf16_map['ERR'])
           scanner.get_byte # ignore the next byte
         when 0xE0..0xFB
           [2, 1, 0].each do |n| # try 3 bytes, then 2 bytes, then 1 byte
-            bytes = [char.to_s(16).upcase]
+            bytes = [char_hex]
             scanner.peek(n).each_byte {|b| bytes << b.to_s(16).upcase}
             hex_key = bytes.join('+')
-            if ansi_to_utf16.has_key?(hex_key)
-              output << ansi_to_utf16[hex_key].force_encoding('UTF-16BE').encode('UTF-8')
+            if @ansi_to_utf16_map.has_key?(hex_key)
+              output << utf16_to_utf8(@ansi_to_utf16_map[hex_key])
               n.times {scanner.get_byte}
               break
             end
           end
         else
-          output << ansi_to_utf16['ERR'].force_encoding('UTF-16BE').encode('UTF-8')
+          output << utf16_to_utf8(@ansi_to_utf16_map['ERR'])
           scanner.get_byte if scanner.get_byte.unpack('C')[0] >= 0xE0 # ignore the next byte
         end
       end
 
-      output.force_encoding('UTF-8')
+      output
     end
 
   end
